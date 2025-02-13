659825002326107bcbc45ab4a897ba86236e1dea
Daria <n-d-p@mail.ru>
2025-02-13 20:25:05 +0300

    ++ prevBtn to NavDays

--- front/client-page/src/components/seances/navDays.jsx
+++ front/client-page/src/components/seances/navDays.jsx
@@ -12,11 +12,13 @@ export default function NavDays({onChange}) {
     const getNextDate = (lastDate) => {
         const nextDay = new Date();
         nextDay.setDate(lastDate.getDate() + 1);
+        nextDay.setHours(0, 0, 0, 0);
         return nextDay;
     };
 
     const dates = [];
     const now = new Date();
+    now.setHours(0, 0, 0, 0);
     dates.push(now);
     let count = 0;
     while (count < maxDays - 1) {
@@ -28,16 +30,26 @@ export default function NavDays({onChange}) {
 
     const [navDays, setNavDays] = useState(dates);
 
-    const updateNavDays = () => {
+    const updateNavDaysToNext = () => {
         const lastDay = navDays[navDays.length - 1];
         const nextDay = getNextDate(lastDay);
+        nextDay.setHours(0, 0, 0, 0);
         const newNavDays = [...navDays, nextDay];
         newNavDays.shift();
         setNavDays(newNavDays);
     };
 
+
+    const updateNavDaysToPrev = () => {
+        const day = new Date(navDays[0].getTime());
+        day.setDate(day.getDate() - 1);
+        day.setHours(0, 0, 0, 0);
+        const newNavDays = [day, ...navDays];
+        newNavDays.pop();
+        setNavDays(newNavDays);
+    };
+
     const changeChooseDay = (dayIndex, date) => {
-        date.setHours(0, 0, 0, 0);
         const string = toISOStringNoMs(date);
         dispatch(changeChosenDate(string));
         onChange(string);
@@ -47,16 +59,19 @@ export default function NavDays({onChange}) {
         return getDateStringFromDate(a) === getDateStringFromDate(b);
     };
 
+
     return (
         <nav className="page-nav">
+            {!isEqual(navDays[0],now) ?
+                <a className="page-nav__day page-nav__day_prev" href="#" onClick={updateNavDaysToPrev}></a> : null}
             {navDays.map((day, index) => <a key={index}
-                                            className={`page-nav__day ${day === now ? "page-nav__day_today" : ""} 
+                                            className={`page-nav__day ${isEqual(day,now) ? "page-nav__day_today" : ""} 
                                             ${isEqual(day, new Date(chosenDate)) ? "page-nav__day_chosen" : ""}`}
                                             href="#" onClick={() => changeChooseDay(index, day)}>
                 <span className="page-nav__day-week">{weekday[day.getDay()]}</span><span
                 className="page-nav__day-number">{day.getDate()}</span>
             </a>)}
-            <a className="page-nav__day page-nav__day_next" href="#" onClick={updateNavDays}>
+            <a className="page-nav__day page-nav__day_next" href="#" onClick={updateNavDaysToNext}>
             </a>
         </nav>
     );
